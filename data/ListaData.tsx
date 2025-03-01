d8dcbdf241cb6e3a7c4682cb4d3661b6473a42c1
Jennyfer56 <rm561020@fiap.com.br>
2025-02-28 22:01:32 -0300

    segundo commit

--- data/ListaData.tsx
+++ data/ListaData.tsx
@@ -13,7 +13,7 @@ export type Item = {
 //criar um array com a lista de compra, contendo varios produtos 
 
 export const listaData: Item[] = [
-    { id: 1, nome: 'Arroz', quantidade: 2 },
+    { id: 1, nome: 'Arroz', quantidade: 3 },
     { id: 2, nome: 'Feijão', quantidade: 2 },
     { id: 3, nome: 'Batata', quantidade: 5 },
     { id: 4, nome: 'Cebola', quantidade: 2 },
@@ -24,7 +24,7 @@ export const listaData: Item[] = [
 ];
 
 export const listaFrutas: Item[] = [
-    { id: 1, nome: 'Melão', quantidade: 2 },
+    { id: 1, nome: 'Melão', quantidade: 3 },
     { id: 2, nome: 'Melancia', quantidade: 2 },
     { id: 3, nome: 'Banana', quantidade: 5 },
     { id: 4, nome: 'Caju', quantidade: 2 },
